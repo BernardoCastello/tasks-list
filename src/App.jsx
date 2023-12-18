d3a643eda3cd4bbc0536bd2f9efa40bc17a3859d
Bernardo Castello <73943159+BernardoCastello@users.noreply.github.com>
2023-12-18 16:32:09 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,62 @@
-/*   App.jsx   */
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import Todo from './components/todo';
 import TodoForm from './components/todoForm';
 
 function App() {
   const [todos, setTodos] = useState([]);
 
-  const addTodo = (text,description, category) => {
-    const newTodos = [...todos, {
-      id: Math.floor(Math.random()*10000),
-      text,
-      description,
-      category,
-      isCompleted: false,
-    },
-  ];
-  setTodos(newTodos);
+  const addTodo = (text, description, category) => {
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        description,
+        category,
+        isCompleted: false,
+      },
+    ];
+    setTodos(newTodos);
   };
 
   const removeTodo = (id) => {
-    const newTodos = [...todos]
-    const filteresTodos = newTodos.filter((todo) => todo.id !== id ? todo : null);
-    setTodos(filteresTodos);
-  }
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const updateTodo = (id, updatedText, updatedDesc, updatedCategory) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          text: updatedText,
+          description: updatedDesc,
+          category: updatedCategory,
+        };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
   return (
     <div className='app'>
       <h1>Lista de Tarefas</h1>
       <div className='todo-list'>
         {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} removeTodo={removeTodo}/>
+          <Todo
+            key={todo.id}
+            todo={todo}
+            updateTodo={updateTodo}
+            removeTodo={removeTodo}
+
+          />
         ))}
       </div>
-      <TodoForm addTodo={addTodo}/>
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 }
 
-export default App
+export default App;
