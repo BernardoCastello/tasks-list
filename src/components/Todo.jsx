5b7bd49f889ae7ea8158220d2f233c778ffb7150
Bernardo Castello <73943159+BernardoCastello@users.noreply.github.com>
2023-12-18 16:32:45 -0300

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,19 +1,63 @@
-/* Todo.jsx  */
-import React from 'react'
+import React, { useState } from 'react';
 
-const Todo = ({ todo, removeTodo}) => {
-  return (
-        <div className='todo'>
-          <div className='content'>
-            <p>{todo.text}</p>
-            <p>{todo.description}</p>
-            <p className='category'>({todo.category})</p>
-          </div>
-          <div>
-            <button className='remove' onClick={() => removeTodo(todo.id)}>X</button>
-          </div>
-        </div>
-      );
+const Todo = ({ todo, removeTodo, updateTodo }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedText, setUpdatedText] = useState(todo.text);
+  const [updatedDesc, setUpdatedDesc] = useState(todo.description);
+  const [updatedCategory, setUpdatedCategory] = useState(todo.category);
+
+  const handleUpdate = () => {
+    updateTodo(todo.id, updatedText, updatedDesc, updatedCategory);
+    setIsEditing(false);
   };
 
-export default Todo
+  return (
+    <div className='todo'>
+      {isEditing ? (
+        <div className='content'>
+          <input
+            type='text'
+            value={updatedText}
+            onChange={(e) => setUpdatedText(e.target.value)}
+          />
+          <input
+            type='text'
+            value={updatedDesc}
+            onChange={(e) => setUpdatedDesc(e.target.value)}
+          />
+          <select
+            value={updatedCategory}
+            onChange={(e) => setUpdatedCategory(e.target.value)}
+          >
+          <option value="">Selecione a Categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
+          <option value="Estudo">Estudo</option>
+          <option value="Compras">Compras</option>
+          <option value="Casa">Casa</option>
+          <option value="Outros">Outros</option>
+          </select>
+          <button onClick={handleUpdate}>Salvar</button>
+        </div>
+      ) : (
+        <div className='content'>
+          <p>{todo.text}</p>
+          <p>{todo.description}</p>
+          <p className='category'>({todo.category})</p>
+        </div>
+      )}
+      <div>
+      {!isEditing && (
+          <button className='edit' onClick={() => setIsEditing(true)}>
+            Editar
+          </button>
+        )}
+        <button className='remove' onClick={() => removeTodo(todo.id)}>
+          X
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
